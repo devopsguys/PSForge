024d2aeb4b71a601e1845e5983194341553bdd13
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-03-27 11:14:36 +0100

    Add Plaster dependency

--- MyGet.ps1
+++ MyGet.ps1
@@ -4,5 +4,8 @@ param(
 )
 
 Import-Module -Force .\PowershellDSCWorkflow.psm1
+Invoke-Paket restore
+
+Import-Module -Force .\packages\Plaster\Plaster.psm1
 
 Export-DSCModule -Version $packageVersion
