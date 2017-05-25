09196cde8dd8eb56eb3d85987a6e4c42a28fa8b8
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-05-25 08:06:51 +0100

    Add the $examples variable into dsc_configuration.ps1 by default. Close #8

--- plaster-powershell-dsc-module/Examples/dsc_configuration.ps1
+++ plaster-powershell-dsc-module/Examples/dsc_configuration.ps1
@@ -6,10 +6,14 @@ configuration default
         [string[]]$NodeName = 'localhost'
     )
 
+    Import-Module <%=${PLASTER_PARAM_project_name}%>
     Import-DSCResource -ModuleName <%=${PLASTER_PARAM_project_name}%>
 
     Node $NodeName
     {
+
+        $moduleRoot = [io.path]::GetDirectoryName((Get-Module <%=${PLASTER_PARAM_project_name}%>).Path)
+        $examples = "$moduleRoot\Examples"
 
         # Install the IIS role
         WindowsFeature IIS
---
+++ plaster-powershell-dsc-module/_dummyModuleFile
@@ -0,0 +1,2 @@
+# Do not delete this file
+# It is necessary in order for the example DSC configuration to import the module, in order to work out its Path
