354f39432124b1c2f3aa56cf3b411b6a23a95fb2
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-14 08:16:11 +0100

    Script that will run in CI

---
+++ CI.ps1
@@ -0,0 +1,8 @@
+$env:PSModulePath = "${PWD}\packages;" + $env:PSModulePath
+
+.\paket\paket.bootstrapper.exe
+.\paket\paket.exe restore
+
+Import-Module .\PSForge.psm1
+
+Invoke-Pester -Path .\PSForge.Tests.ps1 -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
