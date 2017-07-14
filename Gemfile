ac68f2f130913930ffc6ee49302ca01c5d826144
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-14 07:20:09 +0100

    Remove obselete files

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source "https://rubygems.org"
-
-gem "winrm"
-gem "winrm-fs"
-gem "test-kitchen"
-gem "kitchen-azurerm"
-gem "kitchen-dsc", :github => 'elmundio87/kitchen-dsc', :branch => 'master'
-gem "kitchen-vagrant"
-gem "kitchen-pester"
--- appveyor.ps1
+++
@@ -1,7 +0,0 @@
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module Pester -Scope CurrentUser
-Install-Module Plaster -Scope CurrentUser
-Import-Module .\PSForge.psm1
-$res = Invoke-Pester -Path .\PSForge.Tests.ps1 -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
-if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."}
