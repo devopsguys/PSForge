200e1d364054f1212f2ec5fb04ebae8ef70d7f62
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-17 10:05:27 +0100

    Try-Catch to fail builds

--- CI.ps1
+++ CI.ps1
@@ -1,27 +1,33 @@
 
 
-if([Environment]::OSVersion.Platform -eq "Unix"){
-    $env:PSModulePath = "${PWD}/packages:" + $env:PSModulePath
-    if(-not (Test-Path .\paket\paket.exe)){
-        mono .\paket\paket.bootstrapper.exe
+Try {
+
+    if([Environment]::OSVersion.Platform -eq "Unix"){
+        $env:PSModulePath = "${PWD}/packages:" + $env:PSModulePath
+        if(-not (Test-Path .\paket\paket.exe)){
+            mono .\paket\paket.bootstrapper.exe
+        }
+        mono .\paket\paket.exe restore
+    }else{
+        $env:PSModulePath = "${PWD}\packages;" + $env:PSModulePath
+        if(-not (Test-Path .\paket\paket.exe)){
+            .\paket\paket.bootstrapper.exe
+        }
+        .\paket\paket.exe restore
     }
-    mono .\paket\paket.exe restore
-}else{
-    $env:PSModulePath = "${PWD}\packages;" + $env:PSModulePath
-    if(-not (Test-Path .\paket\paket.exe)){
-        .\paket\paket.bootstrapper.exe
-    }
-    .\paket\paket.exe restore
+
+    Import-Module .\PSForge.psm1
+
+    Import-LocalizedData -BaseDirectory "." -FileName "PSForge.psd1" -BindingVariable metadata
+
+    $moduleVersion = $metadata.ModuleVersion
+    $buildNumber = "$moduleVersion-$(Get-Date -Format 'yyyyMMddHHmmss')"
+
+    Write-Host "##vso[task.setvariable variable=moduleversion]${moduleVersion}"
+    Write-Host "##vso[build.updatebuildnumber]${buildNumber}"
+
+    Invoke-Pester -Path .\PSForge.Tests.ps1 -CodeCoverage .\PSForge.psm1 -OutputFormat NUnitXml -OutputFile TestResults.xml -CodeCoverageOutputFile coverage.xml -PassThru -EnableExit
+
+} Catch {
+    Exit 1
 }
-
-Import-Module .\PSForge.psm1
-
-Import-LocalizedData -BaseDirectory "." -FileName "PSForge.psd1" -BindingVariable metadata
-
-$moduleVersion = $metadata.ModuleVersion
-$buildNumber = "$moduleVersion-$(Get-Date -Format 'yyyyMMddHHmmss')"
-
-Write-Host "##vso[task.setvariable variable=moduleversion]${moduleVersion}"
-Write-Host "##vso[build.updatebuildnumber]${buildNumber}"
-
-Invoke-Pester -Path .\PSForge.Tests.ps1 -CodeCoverage .\PSForge.psm1 -OutputFormat NUnitXml -OutputFile TestResults.xml -CodeCoverageOutputFile coverage.xml -PassThru -EnableExit
