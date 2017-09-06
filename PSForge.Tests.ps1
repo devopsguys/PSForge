26cf0761c2e005f2b9d36aca1e46cef1f9b21b4d
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-09-06 13:22:18 +0100

    Test coverage for Test-DSCModule

--- PSForge.Tests.ps1
+++ PSForge.Tests.ps1
@@ -7,12 +7,6 @@ InModuleScope PSForge {
     function Compare-Array {
         $($args[0] -join ",") -eq $($args[1] -join ",")
     }
-        
-    # Describe "PSForge" {
-    #     It "PSForge is available to be imported when called" {
-    #         Get-Module  –ListAvailable | where { $_.Name –eq 'PSForge' } | should not be $null  
-    #     }
-    # }
 
     Describe "OS Detection" {
 
@@ -454,5 +448,60 @@ dependencies
         It "Should download the CACERT file" {
             Assert-MockCalled New-Object -ParameterFilter { $TypeName -eq "System.Net.WebClient" } -Exactly 1 -Scope Describe
         }
+    }
+
+    Describe "Test-DSCModule" {
+        Mock getProjectRoot { "/fake-path"}
+        Mock Push-Location {}
+        Mock Pop-Location {}
+        Mock BootstrapDSCModule {}
+        Mock Invoke-ExternalCommand {}
+        Mock updateBundle {}
+        Mock Invoke-Paket {}
+        Mock Read-Host {}
+       
+        it "Should throw an exception if the credentials file is missing" {
+            Mock Test-Path { $False } -ParameterFilter { $Path -eq "$HOME/.azure/credentials" }
+            { Test-DSCModule } | Should Throw "Create an azure credentials file at /Users/edmundd/.azure/.credentials as described here: https://github.com/test-kitchen/kitchen-azurerm"
+        }
+
+        it "Should prompt the user if the subscription environment variable has not been set" {
+            Mock Test-Path { $True } -ParameterFilter { $Path -eq "$HOME/.azure/credentials" }
+            Mock Test-Path { $False } 
+            
+            Test-DSCModule
+
+            Assert-MockCalled Read-Host -Exactly 1 -Scope It
+        }
+
+        it "Should bootstrap the module dependencies" {
+            Test-DSCModule
+            Assert-MockCalled BootstrapDSCModule -Exactly 1 -Scope It
+        }
+
+        it "Should update the ruby bundle" {
+            Test-DSCModule
+            Assert-MockCalled updateBundle -Exactly 1 -Scope It
+        }
+
+        it "Should pass the correct argument to Test Kitchen by default" {
+            Test-DSCModule
+            Assert-MockCalled Invoke-ExternalCommand -ParameterFilter { $Command -eq "bundle" -and (Compare-Array $Arguments @("exec", "kitchen", "verify")) } -Scope It
+        }
+
+        it "Should pass the correct argument to Test Kitchen if different action is specified" {
+            Test-DSCModule converge
+            Assert-MockCalled Invoke-ExternalCommand -ParameterFilter { $Command -eq "bundle" -and (Compare-Array $Arguments @("exec", "kitchen", "converge")) } -Scope It
+        }
+
+        it "Should throw an exception if invalid action is specified" {
+            { Test-DSCModule invalid } | Should Throw 
+        }
+
+        it "Should pass the correct argument to Test Kitchen if -Debug is used" {
+            Test-DSCModule -Debug
+            Assert-MockCalled Invoke-ExternalCommand -ParameterFilter { $Command -eq "bundle" -and (Compare-Array $Arguments @("exec", "kitchen", "verify", "--log-level","Debug")) } -Scope It
+        }
+
     }
 }
